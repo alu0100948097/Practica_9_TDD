3a01f2f3f1dcda17fabfd242cb8f900db0fb0ff3
alu0100948097 <alu0100948097@ull.edu.es>
2018-05-05 21:36:16 +0100

    Tercer Caso

---
+++ f.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+
+// a*x^2 + b*x +c
+function Raices(a, b, c)
+{
+   // (-b +- math.sqrt((b^2-4ac))/2a;
+   
+   var R1 = (-b + Math.sqrt(b*b -4 * a * c)) / (2* a);
+   var R2 = (-b - Math.sqrt(b*b -4 * a * c)) / (2* a);
+   
+   return [[R1, 0], [R2, 0]];
+}
+assert.deepEqual(Raices(1, -5, 0), [[5, 0], [0, 0]]);
+assert.deepEqual(Raices(2, 5, 0), [[0, 0], [-2.5, 0]]);
+assert.deepEqual(Raices(1, 5, 0), [[0, 0], [-5, 0]]);
