3ae865b559f05fdb38997e45666ddceb5a3a226a
alu0100948097 <alu0100948097@ull.edu.es>
2018-05-05 21:26:10 +0100

    Primer paso del segundo caso

---
+++ c.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+// a*x^2 + b*x +c
+function Raices(a, b, c)
+{
+   return [[5, 0], [0, 0]];
+}
+assert.deepEqual(Raices(1, -5, 0), [[5, 0], [2, 0]]);
+assert.deepEqual(Raices(2, 5, 0), [[0, 0], [-2.5, 0]]);
