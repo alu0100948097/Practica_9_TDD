125745523a0eed6a7435b966cdbd007ecce86690
alu0100948097 <alu0100948097@ull.edu.es>
2018-05-05 21:06:57 +0100

    Casos de Prueba

---
+++ inicio.js
@@ -0,0 +1,9 @@
+// Pruebas, asserts iniciales
+Raices de ax^2 + bx + c = 0 => Raices (a, b, c)
+Raices(1, -5, 0) => Raiz1 = 5; Raiz2 = 2
+Raices(2, 5, 0) => Raiz1 = 0; Raiz2 = -2.5
+Raices(1, 5, 0) => Raiz1 = 0; Raiz2 = -5
+Raices(1, 0, 1) => Raiz1 = 0, li; Raiz2 = 0, -li
+Raices(1, -2, 5) => Raiz1 = 1, 2li; Raiz2 = 1,-2li
+Raices(1, 1, 1) => Raiz1 = -0.5, (Math.sqrt(3)/2)i; Raiz2 = -0.5, (-Math.sqrt(3)/2)i
+Raices(1, -3, 3) => Raiz1 = 1.5, (Math.sqrt(3)/2)i; Raiz2 = 1.5, (-Math.sqrt(3)/2)i
